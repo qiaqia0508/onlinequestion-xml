e514ddaff747e7c61ead191c4e10dc9eae8710f7
chenqian <chenqian@dianwoda.com>
2019-11-14 18:19:01 +0800

    提交

--- src/main/java/com/qiaqia/controller/OnlineProblemRecordController.java
+++ src/main/java/com/qiaqia/controller/OnlineProblemRecordController.java
@@ -5,6 +5,7 @@ import com.qiaqia.dto.APIResponseDto;
 import com.qiaqia.entity.OnlineProblemRecord;
 import com.qiaqia.service.OnlineProblemRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
